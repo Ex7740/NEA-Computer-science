c8e12cac16b1b1a2e92ec40506aef9c5ee06965d
Ex7740 <122614395+Ex7740@users.noreply.github.com>
2026-03-29 16:01:29 +0100

    Final commit before submitting

---
+++ Code/Program_code/Blocks_for_arduino/INO/Arduino_test/Arduino_test.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  while(true) {
+    // code to be executed
+    digitalWrite(12, 1);
+    delay(100);
+    digitalWrite(12, 0);
+    delay(100);
+  }
+}
